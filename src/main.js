ca862985ccf2f5ddc13d112b8dac4c85672ece72
Heszek HEHZ <i.just.need.it.k@gmail.com>
2026-06-01 16:07:38 +0200

    zadanie dodatkowe 2

--- src/main.js
+++ src/main.js
@@ -3,13 +3,14 @@ import javascriptLogo from './assets/javascript.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import { setupCounter } from './counter.js'
+import dayjs from 'dayjs';
 
 const API_URL = 'https://hiueqiuckfldxyjrqzaq.supabase.co/rest/v1/article?select=*';
 const API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhpdWVxaXVja2ZsZHh5anJxemFxIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc4MDMwNDEzOSwiZXhwIjoyMDk1ODgwMTM5fQ.oDpPcftMS19aLBMaJN6mMV57EVUKurDG8pbWart5ZLw';
 
-const fetchArticles = async () => {
+const fetchArticles = async (order = 'created_at.asc') => {
     try {
-        const response = await fetch(API_URL, {
+        const response = await fetch(`${API_URL}&order=${order}`, {
             headers: {
                 apikey: API_KEY,
             }
@@ -33,7 +34,7 @@ const renderArticles = (articles) => {
                 <h2 class="text-xl font-bold">${article.title}</h2>
                 <h3 class="text-gray-500 mb-2">${article.subtitle}</h3>
                 <p class="text-sm text-gray-400 mb-4">
-                    Autor: ${article.author} | Data: ${article.created_at}
+                    Autor: ${article.author} | Data: ${dayjs(article.created_at).format('DD-MM-YYYY')}
                 </p>
                 <p class="text-gray-700">${article.content}</p>
             </div>
@@ -72,62 +73,8 @@ document.getElementById('articleForm').addEventListener('submit', async (e) => {
     fetchArticles();
 });
 
+document.getElementById('sortSelect').addEventListener('change', (e) => {
+    fetchArticles(e.target.value);
+});
 
 fetchArticles();
-
-
-
-// document.querySelector('#app').innerHTML = `
-// <section id="center">
-//   <div class="hero">
-//     <img src="${heroImg}" class="base" width="170" height="179">
-//     <img src="${javascriptLogo}" class="framework" alt="JavaScript logo"/>
-//     <img src="${viteLogo}" class="vite" alt="Vite logo" />
-//   </div>
-//   <div>
-//     <h1>Get started</h1>
-//     <p>Edit <code>src/main.js</code> and save to test <code>HMR</code></p>
-//   </div>
-//   <button id="counter" type="button" class="counter"></button>
-// </section>
-
-// <div class="ticks"></div>
-
-// <section id="next-steps">
-//   <div id="docs">
-//     <svg class="icon" role="presentation" aria-hidden="true"><use href="/icons.svg#documentation-icon"></use></svg>
-//     <h2>Documentation</h2>
-//     <p>Your questions, answered</p>
-//     <ul>
-//       <li>
-//         <a href="https://vite.dev/" target="_blank">
-//           <img class="logo" src="${viteLogo}" alt="" />
-//           Explore Vite
-//         </a>
-//       </li>
-//       <li>
-//         <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-//           <img class="button-icon" src="${javascriptLogo}" alt="">
-//           Learn more
-//         </a>
-//       </li>
-//     </ul>
-//   </div>
-//   <div id="social">
-//     <svg class="icon" role="presentation" aria-hidden="true"><use href="/icons.svg#social-icon"></use></svg>
-//     <h2>Connect with us</h2>
-//     <p>Join the Vite community</p>
-//     <ul>
-//       <li><a href="https://github.com/vitejs/vite" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#github-icon"></use></svg>GitHub</a></li>
-//       <li><a href="https://chat.vite.dev/" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#discord-icon"></use></svg>Discord</a></li>
-//       <li><a href="https://x.com/vite_js" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#x-icon"></use></svg>X.com</a></li>
-//       <li><a href="https://bsky.app/profile/vite.dev" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#bluesky-icon"></use></svg>Bluesky</a></li>
-//     </ul>
-//   </div>
-// </section>
-
-// <div class="ticks"></div>
-// <section id="spacer"></section>
-// `
-
-// setupCounter(document.querySelector('#counter'))
